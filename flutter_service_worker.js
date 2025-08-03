12520cdffa5b618661e29966b510d45f78796203
belalgamal0 <belal.jamal@uvera.co>
2025-08-03 15:02:16 +0300

    new version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9fbfa6935513cb9d37b0e0a979d9e1bc",
+const RESOURCES = {"flutter_bootstrap.js": "68edc4acd411a83aa2a95991fec708b7",
 "version.json": "27789dfa327477b6cb59a60a73db2573",
-"index.html": "cfffcd09fe453d9aeb25c50febc75cb2",
-"/": "cfffcd09fe453d9aeb25c50febc75cb2",
+"index.html": "b9eefd71e4efaaed9075dc0a029d7899",
+"/": "b9eefd71e4efaaed9075dc0a029d7899",
 "firebase-messaging-sw.js": "3843a9a26a3c14da8d3d384a6d7ae9ed",
-"main.dart.js": "de6361ed16dfc58854e4217d56dd82b9",
-"404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
+"main.dart.js": "b86d9f314323c7803edf600ab0f01c6f",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon1.png": "5dcef449791fa27946b3d35ad8803796",
 "favicon%20copy.ico": "755e0d6b2a55c9696c1f433afb62276c",
